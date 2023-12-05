dd8f3f9cc3d9de86a7ef83127ed060b412047f9a
Dev Vrat Pandey <devvratpandey2002@gmail.com>
2023-12-05 18:53:31 +0530

    Blockchain project

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const NFTCONTRACT = "0x83427e5Eb3f5843BDA5138F245D57e15c216dABF";
+export const STAKINGCONTRACT = "0xc8b929dee90f1B6757E1e813c4b837fC136dF3b5"
+export const polygonscanapi = "https://api-testnet.polygonscan.com/api"
+export const moralisapi = "https://deep-index.moralis.io/api/v2/";
+export const nftpng = "https://ipfs.io/ipfs/QmavM8Zpo9bD3r4zEnhbbBLLvHyfr1YL7f1faG3ovaeSSG/";
